ed4b4573cff370ad8ac25c13a5cda25895fe6c1d
Abdullah Naifeh <abdna59@gmail.com>
2022-10-22 11:03:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const main = document.getElementById('main').remove();
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('Id' , 'victory')
+newHeader.innerHTML = 'is the champion';
+document.body.append(newHeader)
